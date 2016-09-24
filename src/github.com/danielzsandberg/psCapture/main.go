de895cb2d624e1b9185943228afd25210db2e523
Daniel Sandberg <dsandber@andrew.cmu.edu>
2016-09-24 13:37:16 -0400

    added ticker

--- src/github.com/danielzsandberg/psCapture/main.go
+++ src/github.com/danielzsandberg/psCapture/main.go
@@ -8,19 +8,27 @@ import (
 )
 
 func main() {
+	ticker := time.Tick(time.Second)
+	for {
+		currentTime := <-ticker
+		executeCommand(currentTime)
+	}
+}
+
+func executeCommand(currentTime time.Time) {
 	cmd := exec.Command("ps")
 	output, err := cmd.Output()
 	if err != nil {
 		fmt.Printf("%v", err)
 	}
 
-	wrappedOutput := wrapOutput(string(output))
+	wrappedOutput := wrapOutput(string(output), currentTime)
 
 	fmt.Println(wrappedOutput)
 }
 
-func wrapOutput(psOutput string) string {
-	currentTime := time.Now().String()
+func wrapOutput(psOutput string, timeOfExecution time.Time) string {
+	currentTime := timeOfExecution.String()
 
 	firstLine := fmt.Sprintf("********%v*******\n", currentTime)
 	buffer := bytes.NewBufferString(firstLine)
